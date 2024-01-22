3492a6a9bcc4e3dd3ac17b49dab9d377cbf7204d
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2024-01-22 13:41:53 +0100

    Composable middleware chains

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,16 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // Update the signature for the routes() method so that it returns a
 // http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	// Wrap the existing chain with the recoverPanic middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
+
 }
